5bc9a11fc49140dd3d9ed8b7d5e6bb50d01fad22
Mira <calopter@gmail.com>
2020-03-29 20:44:27 -0700

    consume the feed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('hello world');
+const FeedParser = require('feedparser');
+const request = require('request');
+ 
+const req = request('http://friendsatthetable.net/rss');
+const feedparser = new FeedParser();
+
+function parseDur (ep) {
+  if (!ep) return;
+  
+  let [h, m, s] = ep['itunes:duration']['#'].split(':').map(t => +t);
+  
+  if (!s) {
+    s = m;
+    m = h;
+    h = null;
+  }
+  
+  return [h, m, s];
+}
+ 
+req.on('response', function (res) {
+  const stream = this;
+ 
+  if (res.statusCode !== 200) {
+    this.emit('error', new Error('Bad status code'));
+  }
+  else {
+    stream.pipe(feedparser);
+  }
+});
+ 
+feedparser.on('error', function (error) {
+  // always handle errors
+});
+ 
+feedparser.on('readable', function () {
+  const stream = this;
+  let item;
+ 
+  while (item = stream.read()) {
+    console.log(parseDur(item));
+  }
+});
